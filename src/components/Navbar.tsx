fdbcc2c6fb064f0efdd660ec7628e930c37d7c93
Parth Agrawal <pvagrawal0@gmail.com>
2024-07-02 18:41:25 -0400

    added avatar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,40 @@
 "use client"
+import { SignInButton, SignedIn, SignedOut, useAuth, useClerk } from "@clerk/nextjs"
 import clsx from "clsx"
 import { Link } from "lucide-react"
 import { usePathname } from 'next/navigation'
+import Button from "./Button"
+import Avatar from "./Avatar"
 
 export const Navbar = () => {
 
     const path = usePathname()
+    const clerk = useClerk()
+
+    const avatarImage = clerk.user?.imageUrl
+
+
+
+
+
 
     return (
-        <div>
-            <div className="tracking-[2px] flex flex-row w-full text-3xl items-start border-b-4 border-black p-5">
+        <div className="flex flex-row w-full justify-between border-b-4 border-black p-5" >
+            <div className="tracking-[2px] flex flex-row w-full text-3xl items-start ">
                 ENDLESS FORMS MOST BEAUTIFUL
+            </div>
+            <div>
+                <SignedOut>
+
+                    <SignInButton>
+                        <Button onClick={() => { }}>Sign in</Button>
+                    </SignInButton>
+                </SignedOut>
+                <SignedIn>
+                    <Avatar imageUrl={avatarImage || ""} />
+
+                </SignedIn>
+
             </div>
 
 
